527dd1da39c13c17b3afca7f70294eb6b17b1042
akamushi <koorijiman+github@gmail.com>
2018-11-18 16:38:25 +0900

    B-1 カウントダウン

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 var example = []string{"golang", "hands-on", "in", "kagawa"}
 
@@ -10,6 +13,8 @@ func main() {
 		ok, ng int
 	)
 	data = example
+
+	countdown()
 
 	for _, v := range data {
 		fmt.Println(v)
@@ -31,4 +36,11 @@ func main() {
 	fmt.Printf("[正誤] 正解: %d, 誤り: %d\n", ok, ng)
 	cnt := float64(len(data))
 	fmt.Printf("正解率: %f, 誤り率: %f\n", float64(ok)/cnt, float64(ng)/cnt)
+}
+
+func countdown() {
+	for i := 0; i < 3; i++ {
+		fmt.Println(3 - i)
+		time.Sleep(time.Second * 1)
+	}
 }
