7e698b8ae0b429a5f40ec134837b5177aae99dcc
akamushi <koorijiman+github@gmail.com>
2018-11-21 06:30:18 +0900

    D-1 ファイル読み込み

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"time"
 )
 
 var example = []string{"golang", "hands-on", "in", "kagawa"}
 
 func main() {
+	filename := "./example.txt"
+
+	if _, err := os.Stat(filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	data, err := output(filename)
+	if err != nil {
+		os.Exit(1)
+	}
+	startGame(data)
+}
+
+func startGame(data []string) {
 	var (
-		data      []string
 		ok, ng    int
 		inputTime float64
 	)
-	data = example
-
 	countdown()
 
 	for _, v := range data {
@@ -43,11 +56,28 @@ func main() {
 	cnt := float64(len(data))
 	fmt.Printf("正解率: %f, 誤り率: %f\n", float64(ok)/cnt, float64(ng)/cnt)
 	fmt.Printf("平均入力時間: %f\n", float64(inputTime)/cnt)
-}
 
+}
 func countdown() {
 	for i := 0; i < 3; i++ {
 		fmt.Println(3 - i)
 		time.Sleep(time.Second * 1)
 	}
+}
+
+func output(filename string) ([]string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	var texts []string
+	scan := bufio.NewScanner(f)
+	for scan.Scan() {
+		text := scan.Text()
+		texts = append(texts, text)
+	}
+	return texts, nil
 }
